010c78d61a812a36d4e130c661f61d6ba2ba78e9
Justine Okumu <okumujustine01@gmail.com>
2025-03-06 22:53:19 -0600

    adds show guesser

--- frontend/backoffice/src/components/AdminApp.tsx
+++ frontend/backoffice/src/components/AdminApp.tsx
@@ -1,5 +1,5 @@
 import { dataProvider } from "@/client/mock/privider";
-import { Admin, EditGuesser, ListGuesser, Resource } from "react-admin";
+import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser } from "react-admin";
 
 const AdminApp = () => {
   type roleType = "admin" | "propertyOwner";
@@ -11,12 +11,14 @@ const AdminApp = () => {
           name="users"
           list={ListGuesser}
           edit={EditGuesser}
+          show={ShowGuesser}
         />
       ) : (
         <Resource
           name="posts"
           list={ListGuesser}
           edit={EditGuesser}
+          show={ShowGuesser}
         />
       )}
     </Admin>
